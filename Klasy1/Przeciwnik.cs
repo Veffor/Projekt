ff946888b929b7a63cd6fc66ba092252f7fb9cbc
Meteor133 <mateuszufh@gmail.com>
2023-03-12 15:16:15 +0100

    zrobiona część logiki

---
+++ Klasy1/Przeciwnik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasy1
+{
+    public class Przeciwnik
+    {
+        private int hp_przeciwnika;
+        private int atak_przciwnika;
+        private string nazwa_przeciwnika;
+
+        public Przeciwnik(int hp_przeciwnika, int atak_przciwnika, string nazwa_przeciwnika)
+        {
+            this.hp_przeciwnika = hp_przeciwnika;
+            this.atak_przciwnika = atak_przciwnika;
+            this.nazwa_przeciwnika = nazwa_przeciwnika;
+        }
+
+        public int Hp_przeciwnika { get => hp_przeciwnika; set => hp_przeciwnika = value; }
+        public int Atak_przciwnika { get => atak_przciwnika; set => atak_przciwnika = value; }
+        public string Nazwa_przeciwnika { get => nazwa_przeciwnika; set => nazwa_przeciwnika = value; }
+    }
+}
--- Projekt/MainWindow.xaml.cs
+++ Projekt/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Klasy1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,54 @@ namespace Projekt
     {
         public MainWindow()
         {
+            Random r1 = new Random();
+            int atak_gracza1 = r1.Next(10, 50);
+            int atak_przeciwnika = r1.Next(5, 40);
+
             InitializeComponent();
+            int hp_gracza = 100;
+            int hp_przeciwnika = 100;
+
+            string nazwa_gracza = "gracz numer 1";
+            string nazwa_przeciwnika = "przeciwnik numer 1";
+
+            Gracz gracz1 = new(hp_gracza,20,nazwa_gracza, atak_gracza1);
+            Przeciwnik przeciwnik1 = new(hp_przeciwnika, atak_przeciwnika, nazwa_przeciwnika);
+           
+            hp_twoje_zmienne.Content = hp_gracza;
+            hp_przeciwnika_zmienne.Content = hp_przeciwnika;
+            nazwa_twoja.Text = nazwa_gracza;
+            nazwa_przeciwnika_okienko.Text = nazwa_przeciwnika;
+
+
+            BitmapImage obrazek = new BitmapImage();
+            obrazek.BeginInit();
+            
+            
+
+
+                int przeciwnik_wyświetlany = r1.Next(1, 3); // ten switch tak działa w jednej czwartej
+
+                switch (przeciwnik_wyświetlany)
+                {
+                    case 1:
+                    obrazek.UriSource = new Uri("/enemy1.png", UriKind.Relative);
+                        break;
+                    case 2:
+                    obrazek.UriSource = new Uri("/enemy2.png", UriKind.Relative);
+                    break;
+                    
+                   
+                }
+            obrazek.EndInit();
+            zdj_przeciwnika.Source = obrazek;
+
+
+        }
+
+        private void button_atak_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
